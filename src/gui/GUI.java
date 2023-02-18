246bfaf4e9a4c6773bde065f800d38cb06a12b50
LouisLaptop <23827033@sun.ac.za>
2023-02-18 14:45:50 +0200

    gui works, first draft for login screen

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -3,6 +3,7 @@ package gui;
 //TODO: decide what gui components to use, then add/remove relevant imports
 
 import javax.swing.*;
+import java.awt.event.*;
 
 public class GUI 
 {
@@ -13,14 +14,39 @@ public class GUI
     //gui constructor
     public GUI() 
 	{
+		////////////////////text box////////////////////////////
+		JTextField un = new JTextField();
+		un.setBounds(50, 50, 150, 30);
+		///////////////////////////////////////////////////////
+
+		////////////////////button////////////////////////////
+		JButton b = new JButton("Log in");
+		b.setBounds(75, 100, 100, 30);
+		//b.addActionListener(new ActionListener() {
+			//public void actionPerformed(ActionEvent e) {
+			
+			//}
+		//});
+		////////////////////////////////////////////////////////
+
+		//////////////////label////////////////////////////////
+		JLabel l = new JLabel("Enter Username");
+		l.setBounds(75, 20, 100, 30);
+		///////////////////////////////////////////////////////
+
+		/////////////////////frame adds//////////////////////////
+		frame.add(un);
+		frame.add(b);
+		frame.add(l);
+		////////////////////////////////////////////////////////
+
+		//////////////////////////frame code//////////////////////
 	 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       	frame.setSize(250, 200);
 	  	frame.setLayout(null);
 	  	frame.setVisible(true);
 		frame.setLocationRelativeTo(null);
-
-      	//TODO: initialize gui components here
-
+		//////////////////////////////////////////////////////////
     }
 }
 
