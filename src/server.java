c737046de2bc6584f95bfcdd0a4afc0896587a98
Jean-Luc <24810142@sun.ac.za>
2023-02-18 10:26:04 +0200

    Added methods for checking validity of username + list request function for clients

--- src/server.java
+++ src/server.java
@@ -3,7 +3,7 @@ import java.net.*;
 import java.util.ArrayList;
 
 class server extends Thread{
-	ArrayList<String> user_list = new ArrayList<String>();
+	private ArrayList<String> user_list = new ArrayList<String>();
 
     public void run() {
 
@@ -32,26 +32,29 @@ class server extends Thread{
     }
 
 	/**
-	* Private method for the server to update the active user list.
-	*
-	* @param action add/remove user, corresponding to values of 0 and 1
-	* 				respectively
-	*
-	* @param username User upon which action should take place
-	*
-	*/
+	 * Private method for the server to update the active user list, also
+	 * prints corresponding actions to terminal.
+	 *
+	 * @param action add/remove user, corresponding to values of 0 and 1
+	 * 				respectively
+	 *
+	 * @param username User upon which action should take place
+	 *
+	 */
 	private static void update_list(int action, String username) {
 		//Possibly switch over to if/else, using switch for safety with default
 
 		switch(action):
 			case 0:
 				user_list.add(username);
+				System.out.println("'" + username + "' connected");
 				break;
 
 			case 1:
 				for (int i = 0; i < user_list.size(); i++) {
 					if (user_list.get(i).equals(username)) {
 						user_list.remove(i);
+						System.out.println("'" + username + "' disconnected");
 						break;
 					}
 				}
@@ -60,5 +63,36 @@ class server extends Thread{
 			default:
 				System.out.println("Invalid operation value");
 				break;
+	}
+
+	/**
+	 * Private method for server to determine uniqueness of client username
+	 *
+	 * @param username unique id with which user attempts to connect
+	 * @return boolean determining if username is available (true if so)
+	 *
+	 */
+	private static boolean check_list(String username) {
+		boolean valid = true;
+
+		for (int i = 0; i < user_list.size(); i++) {
+			if (user_.list.get(i).equals(username)) {
+				valid = false;
+				break;
+			}
+		}
+
+		return valid;
+	}
+
+	/**
+	 * Public method for client to request the user list.
+	 *
+	 * @return Arraylist containing connected users
+	 *
+	 */
+	public static ArrayList<String> request_list() {
+		// Very placeholder, determine how requests will be handled via sockets.
+		return	user_list;
 	}
 }
